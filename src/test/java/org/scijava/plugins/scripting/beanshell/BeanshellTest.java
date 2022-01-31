af74e4549510bc85887c263f63f5b5cb52cfd92e
Curtis Rueden <ctrueden@wisc.edu>
2022-01-31 12:06:46 -0600

    Use the provided BshScriptEngine
    We tried this once before (f17c2fd530375158c2f22a58add367c49215e6c7),
    but it didn't take due to a bug in the engine surrounding nulls. But
    this version of BeanShell, 2.1.0, no longer suffers from that issue.
    
    Thanks to @subes for bringing it to attention.
    
    Closes #2.
    
    Co-authored-by: Mark Hiner <hinerm@gmail.com>

--- src/test/java/org/scijava/plugins/scripting/beanshell/BeanshellTest.java
+++ src/test/java/org/scijava/plugins/scripting/beanshell/BeanshellTest.java
@@ -48,6 +48,8 @@ import org.scijava.script.ScriptLanguage;
 import org.scijava.script.ScriptModule;
 import org.scijava.script.ScriptService;
 
+import bsh.BshScriptEngine;
+
 /**
  * Unit tests for the Beanshell support.
  * 
@@ -80,14 +82,14 @@ public class BeanshellTest extends AbstractScriptLanguageTest {
 
 		final ScriptLanguage language = scriptService.getLanguageByExtension("bsh");
 		final ScriptEngine engine = language.getScriptEngine();
-		assertEquals(BeanshellScriptEngine.class, engine.getClass());
+		assertEquals(BshScriptEngine.class, engine.getClass());
 		engine.put("hello", 17);
 		assertEquals("17", engine.eval("hello").toString());
 		assertEquals("17", engine.get("hello").toString());
 
 		final Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
 		bindings.clear();
-		assertEquals("void", engine.get("hello").toString());
+		assertEquals(null, engine.get("hello"));
 	}
 
 	@Test
@@ -98,14 +100,14 @@ public class BeanshellTest extends AbstractScriptLanguageTest {
 		final ScriptService scriptService = context.getService(ScriptService.class);
 
 		final String script = "" + //
-			"// @ScriptService ss\n" + //
-			"// @OUTPUT String language\n" + //
-			"language = ss.getLanguageByName(\"Beanshell\").getLanguageName();\n";
+			"#@ ScriptService ss\n" + //
+			"#@output String language\n" + //
+			"language = ss.getLanguageByName(\"BeanShell\").getLanguageName();\n";
 		final ScriptModule m = scriptService.run("hello.bsh", script, true).get();
 
 		final Object actual = m.getOutput("language");
 		final String expected =
-			scriptService.getLanguageByName("Beanshell").getLanguageName();
+			scriptService.getLanguageByName("BeanShell").getLanguageName();
 		assertEquals(expected, actual);
 
 		final Object result = m.getReturnValue();
